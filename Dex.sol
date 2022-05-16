f4bffe3e40b5530beec837f2ec5377eb28eb1a20
debeldami <debeldamilola@gmail.com>
2022-05-16 13:32:57 +0100

    token exsit modifier

--- Dex.sol
+++ Dex.sol
@@ -1,4 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol';
 
-contract Dex{}
+contract Dex{
+    struct Token{
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    mapping(bytes32 => Token) public token;
+
+    mapping(address => mapping(bytes32 => uint)) public tradersBalances;
+
+    bytes32[] public tokenList;
+
+    address public admin;
+
+    constructor(){
+        admin = msg.sender;
+    }
+
+    function addToken (bytes32 ticker, address tokenAddress) external onlyAdmin{
+        token[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    function deposit(uint amount, bytes32 ticker) external{
+        IERC20(token[ticker].tokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+    }
+
+    modifier onlyAdmin{
+        require(msg.sender == admin, "only admin");
+        _;
+    }
+
+     modifier tokenExist(bytes32 ticker){
+        require(token[ticker].tokenAddress != address(0), "token does not exist");
+        _;
+    }
+}
