da68e83c267c2a822abdeb5004f8209cb6010061
debeldami <debeldamilola@gmail.com>
2022-05-19 17:09:31 +0100

    Zrx token

--- Zrx.sol
+++ Zrx.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.13;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract Bat is ERC20{
-    constructor() ERC20('Bat', 'Basic attention token'){}
+contract Zrx is ERC20{
+    constructor() ERC20('ZRX', '0x token'){}
 }
