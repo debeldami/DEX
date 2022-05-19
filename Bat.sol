62db14c7720d5c691b272f052728853f3f25d009
debeldami <debeldamilola@gmail.com>
2022-05-19 16:49:26 +0100

    dai rep bat

---
+++ Bat.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Bat is ERC20{
+    constructor() ERC20('Bat', 'Basic attention'){}
+}
