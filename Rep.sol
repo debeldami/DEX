20dbfe8e3bff76adde2f6e350950bd5965d68680
debeldami <debeldamilola@gmail.com>
2022-05-22 15:44:18 +0100

    faucet included to bat dai rep zrx

--- Rep.sol
+++ Rep.sol
@@ -3,5 +3,10 @@ pragma solidity ^0.8.13;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract Rep is ERC20{
+
     constructor() ERC20('Rep', 'Augur'){}
+
+    function faucet(address to, uint amount) external {
+        _mint(to, amount);
+    }
 }
--- Zrx.sol
+++ Zrx.sol
@@ -3,5 +3,11 @@ pragma solidity ^0.8.13;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract Zrx is ERC20{
+    
     constructor() ERC20('ZRX', '0x token'){}
+
+    function faucet(address to, uint amount) external {
+        _mint(to, amount);
+    }
+
 }
