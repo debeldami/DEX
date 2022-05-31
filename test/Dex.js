d982bee92a7ef09bb46a22eda322c5b5ec9977b0
debeldami <debeldamilola@gmail.com>
2022-05-31 15:04:30 +0100

    test create market order unhappy path

--- test/Dex.js
+++ test/Dex.js
@@ -263,4 +263,46 @@ contract('Dex', (accounts) => {
     assert(balances[2].toString() === web3.utils.toWei('50'));
     assert(balances[3].toString() === web3.utils.toWei('95'));
   });
+
+  it('should NOT create market order if token balance too low', async () => {
+    await expectRevert(
+      dex.createMarketOrder(REP, web3.utils.toWei('101'), SIDE.SELL, {
+        from: trader2,
+      }),
+      'token balance too low'
+    );
+  });
+
+  it('should NOT create market order if dai balance too low', async () => {
+    await dex.deposit(web3.utils.toWei('100'), REP, { from: trader1 });
+
+    await dex.createLimitOrder(REP, web3.utils.toWei('100'), 10, SIDE.SELL, {
+      from: trader1,
+    });
+
+    await expectRevert(
+      dex.createMarketOrder(REP, web3.utils.toWei('101'), SIDE.BUY, {
+        from: trader2,
+      }),
+      'dai balance too low'
+    );
+  });
+
+  it('should NOT create market order if token is DAI', async () => {
+    await expectRevert(
+      dex.createMarketOrder(DAI, web3.utils.toWei('1000'), SIDE.BUY, {
+        from: trader1,
+      }),
+      'Cannot trade DAI'
+    );
+  });
+
+  it('should NOT create market order if token does not not exist', async () => {
+    await expectRevert(
+      dex.createMarketOrder(ZRX, web3.utils.toWei('1000'), SIDE.BUY, {
+        from: trader1,
+      }),
+      'token does not exist'
+    );
+  });
 });
